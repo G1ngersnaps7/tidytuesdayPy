83a3b20a3f921a07ff557a8b1c48940735a2e303
Sarah Jeffrey <jsarahj@gmail.com>
2026-05-08 09:56:10 -0600

    Created 2026 wk 5 folder and initial tidytusday py script

---
+++ 2026/week_05_edible_plants/TidyTuesday_20260203.py
@@ -0,0 +1,10 @@
+# TidyTuesday 2026 Week 4: Edible Plants
+
+# Plan: What plants are 'least demanding' overall to grow - for the 
+# green-thumb challenged people. 
+
+# --1. Load libraries --
+import pandas as pd
+
+# ---2. Get TT data from GitHub --
+edible_plants_raw = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-02-03/edible_plants.csv')
