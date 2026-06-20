a51763f035ee25e15f93159964a16316d1eecb1c
Sarah Jeffrey <jsarahj@gmail.com>
2026-06-20 13:04:06 -0600

    Created TT Week 12 folder and inital py script

---
+++ 2026/week_12_digits_of_pi/TidyTuesday_20260324.py
@@ -0,0 +1,9 @@
+# TidyTuesday week 12 - March 24, 2026
+## One Million Digits of Pi 
+
+
+# -- 1. Libraries ----
+#from inspect import CO_VARKEYWORDS
+import pandas as pd
+import matplotlib.pyplot as plt 
+import statsmodels.formula.api as smf  
