f92174998390ab55bedc5d701791dc218bcb43f1
Sarah Jeffrey <jsarahj@gmail.com>
2026-06-20 14:13:25 -0600

    Added code to generate the pi steps for each digit with  object

--- 2026/week_12_digits_of_pi/TidyTuesday_20260324.py
+++ 2026/week_12_digits_of_pi/TidyTuesday_20260324.py
@@ -1,9 +1,27 @@
 # TidyTuesday week 12 - March 24, 2026
 ## One Million Digits of Pi 
 
+# Visualize the digits of pi via a pi walk - assign 
+#  map each number to a direction representing cardinal 
+# and intercardinal directions 
+
 
 # -- 1. Libraries ----
-#from inspect import CO_VARKEYWORDS
 import pandas as pd
-import matplotlib.pyplot as plt 
-import statsmodels.formula.api as smf  
+import numpy as np
+
+pi_digits = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2026/2026-03-24/pi_digits.csv')
+
+# -- 2. Create directions ----
+
+# 8 angles, 45 degrees apart in radians 
+angles = np.linspace(0, 2*np.pi, 8, endpoint=False)
+# create an 8 x 2 array with x/y movements
+directions = np.column_stack([np.cos(angles), np.sin(angles)])  
+# get remainder of each digit after dividing by 8 (to have 8 values to 
+# match 8 directions)
+digit_directions = pi_digits['digit'] % 8
+# look up the actual (x,y) movement for each digit, using digit_directions
+# as a row index in directions array to make one x,y step per
+# digit in pi, in order
+steps = directions[digit_directions]
