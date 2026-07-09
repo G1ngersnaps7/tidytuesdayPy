5deaad9132279aade298f5a438329a62f7a12d34
Sarah Jeffrey <jsarahj@gmail.com>
2026-07-09 07:43:32 -0600

    added step to create positions for pi digits to turn into plot. Next step will be plotting

--- 2026/week_12_digits_of_pi/TidyTuesday_20260324.py
+++ 2026/week_12_digits_of_pi/TidyTuesday_20260324.py
@@ -20,12 +20,20 @@ pi_digits = pd.read_csv('https://raw.githubusercontent.com/rfordatascience/tidyt
 
 # 8 angles, 45 degrees apart in radians 
 angles = np.linspace(0, 2*np.pi, 8, endpoint=False)
+
 # create an 8 x 2 array with x/y movements
 directions = np.column_stack([np.cos(angles), np.sin(angles)])  
+
 # get remainder of each digit after dividing by 8 (to have 8 values to 
 # match 8 directions)
 digit_directions = pi_digits['digit'] % 8
+
 # look up the actual (x,y) movement for each digit, using digit_directions
 # as a row index in directions array to make one x,y step per
 # digit in pi, in order
 steps = directions[digit_directions]
+
+# take each step and turn them into an array of moves to plot
+# the path for the pi walk. Each position is just a sum of all 
+# previous positions. 
+path = np.cumsum(steps, axis=0) #axis=0 to sum row by row, down each col
